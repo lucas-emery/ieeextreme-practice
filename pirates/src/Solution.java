c77f97a68d3e1c0849ed33bd960a76fa1d2c0860
lucas <lucas.emery@hotmail.com>
2017-09-25 22:51:10 -0300

    Pirates - Parser v1

--- pirates/src/Solution.java
+++ pirates/src/Solution.java
@@ -16,65 +16,108 @@ public class Solution {
         int m = Integer.parseInt(tk.nextToken());
         int q = Integer.parseInt(tk.nextToken());
 
-        int[][] map = new int[n][m];
+        //int[] map = new int[n*m];
 
+        Graph<Integer> map = new Graph<>();
+
+        String input;
+        boolean cloned;
+        boolean island;
+        int k;
         for (int i = 0; i < n; i++) {
-            tk = new StringTokenizer(in.readLine());
+            //tk = new StringTokenizer(in.readLine());
+            input = in.readLine();
             for (int j = 0; j < m; j++) {
-                //crete map
+                k = (i*m)+j;
+                cloned = false;
+
+                //island = tk.nextToken().equals("O");
+                island = input.charAt(j) == 'O';
+
+                if(j > 0) {
+                    cloned = map.cloneNode(k - 1, k, island);
+
+                    if (cloned)
+                        map.mergeNode(k - m - 1, k);
+                    else
+                        cloned = map.cloneNode(k - m - 1, k, island);
+                }
+
+                if(cloned)
+                    map.mergeNode(k - m, k);
+                else
+                    cloned = map.cloneNode(k - m, k, island);
+
+                if(!cloned)
+                    map.addNode(k, island);
             }
         }
+
+        System.out.println(map.toString(n, m));
     }
 
-    private class Graph<V> {
+    static private class Graph<V> {
 
         HashMap<V, Node> nodes;
 
         private class Node {
             Set<Node> neighbors;
+            Set<V> keys;
             boolean visited;
             boolean island;
 
             Node(boolean island) {
                 neighbors = new HashSet<>();
+                keys = new HashSet<>();
                 this.island = island;
                 visited = false;
             }
         }
 
-        public void addNode(V v, boolean island) {
+        public Graph() {
+            nodes = new HashMap<>();
+        }
+
+        public boolean addNode(V v, boolean island) {
             if(nodes.containsKey(v))
-                return;
+                return false;
 
             Node n = new Node(island);
             nodes.put(v, n);
+            n.keys.add(v);
+
+            return true;
         }
 
-        public void cloneNode(V v, V w) {
+        public boolean cloneNode(V v, V w, boolean island) {
             Node n = nodes.get(v);
-            if(n == null)
-                return;
+            if(n == null || n.island != island)
+                return false;
 
             if(nodes.get(w) != null)
                 mergeNode(v, w);
-            else
+            else {
                 nodes.put(w, n);
+                n.keys.add(w);
+            }
 
-
+            return true;
         }
 
         public void mergeNode(V v, V w) {
             Node n1 = nodes.get(v);
             Node n2 = nodes.get(w);
-            if(n1.island == n2.island) {
+            if(n1 != null && n2 != null && n1 != n2 && n1.island == n2.island) {
                 n2.neighbors.remove(n1);
                 for (Node n : n2.neighbors) {
                     n.neighbors.remove(n2);
                     n.neighbors.add(n1);
                 }
+                for (V key : n2.keys) {
+                    nodes.put(key, n1);
+                }
                 n1.neighbors.addAll(n2.neighbors);
-                nodes.remove(w);
-                nodes.put(w, n1);
+                n1.keys.addAll(n2.keys);
             }
         }
 
@@ -90,6 +133,32 @@ public class Solution {
             Node n2 = nodes.get(w);
             n1.neighbors.remove(n1);
             n2.neighbors.remove(n2);
+        }
+
+        public String toString(int height, int width) {
+            StringBuilder sb = new StringBuilder();
+            HashMap<Node, Integer> labels = new HashMap<>();
+
+            int nextLabel = 1;
+            int label;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    Node n = nodes.get((i*width)+j);
+                    if(labels.get(n) == null) {
+                        label = nextLabel;
+                        labels.put(n, label);
+                        nextLabel++;
+                    }
+                    else {
+                        label = labels.get(n);
+                    }
+                    sb.append(label);
+                    sb.append(' ');
+                }
+                sb.append('\n');
+            }
+
+            return sb.toString();
         }
     }
 }
